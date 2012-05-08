7a29154d9aa6aaa694c7aba724272838c9d6c34f
Th3D0c <Th3D0c@thedochouse.org>
2012-05-08 02:39:09 +0200

    correction url redirection bouton validation de doublon JS

--- public/javascripts/include/import.js
+++ public/javascripts/include/import.js
@@ -3,7 +3,7 @@ function validateDoublon(word_id) {
         $.get("/wordsAnalyzer/import/validateDouble/" + word_id, function(result){
             if(result == 'true') {
                 alert("Word #" + word_id +  " successfully validated");
-                window.location.href = "/wordsAnalyzer/import/validate";
+                window.location.href = "/wordsAnalyzer/import/checkDoublons";
             } else {
                 alert("Can not validate word #" + word_id);
                 console.log('Error returned is: ' + result);
